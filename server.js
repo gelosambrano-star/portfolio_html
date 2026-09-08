fb830197431ed93c317ad6206881f8d816df1ff7
Angelo Sambrano <gelosambrano@gmail.com>
2026-09-08 23:21:22 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const mongoose = require('mongoose'); // Handles the database pipeline connection
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 
 // 🛑 STEP 1: Paste the connection string you copied from the MongoDB Drivers modal here:
